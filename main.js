b34fcfdfa9a9e3564c02b3bf05b89ca284da43fa
kbelim <106183879+kbelim@users.noreply.github.com>
2024-06-14 08:54:48 -0300

    14/06

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 const botoes = document.querySelectorAll(".botao");
 const conteudo = document.querySelectorAll(".aba-conteudo")
+const contador = document.querySelectorAll(".contador")
+const tempoobjetivo1= new Date(2024,5,14,8,50,0)
+const tempoobjetivo2= new Date (2024,7,22,8,50,0)
+const tempoobjetivo3= new Date (2024,0,1,10,0,0)
+const tempoobjetivo4= new Date (2024,1,1,1,1,1)
+contador[0].textContent="olá"
+
 
 for (let i = 0 ; i < botoes.length ; i++){
     botoes[i].onclick = function (){
